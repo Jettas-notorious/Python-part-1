85e54202413d12f73cec8edbce65a10d3c77af9c
Jettas-notorious <jetrocleophas1@gmail.com>
2025-08-20 15:12:02 +0200

    newby

---
+++ Module exercises/Module 2/input and output.py
@@ -0,0 +1,13 @@
+a=5.0
+b=10.5
+
+answer=a+b
+print("The answer is =", answer)
+sub=b-a
+print("This is a sunbtracted answer:", sub)
+
+mul=a*b
+print("This is a multiply answer:",mul)
+
+div=b/a
+print("this is a divided answer:", div)
